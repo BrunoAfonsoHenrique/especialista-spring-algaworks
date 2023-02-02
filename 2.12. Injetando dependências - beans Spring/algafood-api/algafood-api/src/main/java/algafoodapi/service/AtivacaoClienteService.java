ffa4f53d47209c8c1a8ee86473e8ece07fc2781d
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-02 11:50:46 -0300

    codigo-fonte da aula

---
+++ 2.12. Injetando dependências - beans Spring/algafood-api/algafood-api/src/main/java/algafoodapi/service/AtivacaoClienteService.java
@@ -0,0 +1,24 @@
+package algafoodapi.service;
+
+import algafoodapi.modelo.Cliente;
+import algafoodapi.notificacao.Notificador;
+import algafoodapi.notificacao.NotificadorEmail;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+
+    private Notificador notificador;
+
+    public AtivacaoClienteService(Notificador notificador) {
+        this.notificador = notificador;
+
+        System.out.println("AtivacaoClienteService: " + notificador);
+    }
+
+    public void ativar(Cliente cliente) {
+        cliente.ativar();
+
+        notificador.notificar(cliente, "Seu cadastro no sistema esta ativo!");
+    }
+}
