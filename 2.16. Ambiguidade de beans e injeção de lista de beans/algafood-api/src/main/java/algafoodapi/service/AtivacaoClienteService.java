54b2cf26f52bd19a27e1e59af72a866571ee5b26
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-03 12:04:51 -0300

    codigo-fonte da aula

--- 2.16. Ambiguidade de beans e injeção de lista de beans/algafood-api/src/main/java/algafoodapi/service/AtivacaoClienteService.java
+++ 2.16. Ambiguidade de beans e injeção de lista de beans/algafood-api/src/main/java/algafoodapi/service/AtivacaoClienteService.java
@@ -5,19 +5,19 @@ import algafoodapi.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired(required = false)
-    private Notificador notificador;
+    @Autowired
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        if (notificador != null) {
+        for(Notificador notificador : notificadores) {
             notificador.notificar(cliente, "Seu cadastro no sistema esta ativo!");
-        } else {
-            System.out.printf("Não existe notificador, mas cliente foi ativado");
         }
 
     }
