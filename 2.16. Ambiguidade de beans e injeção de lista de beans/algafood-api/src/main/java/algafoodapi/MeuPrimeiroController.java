6e86366c3c442eb44b6011341897b519d8badc86
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-03 11:49:50 -0300

    codigo-fonte da aula

---
+++ 2.16. Ambiguidade de beans e injeção de lista de beans/algafood-api/src/main/java/algafoodapi/MeuPrimeiroController.java
@@ -0,0 +1,26 @@
+package algafoodapi;
+
+import algafoodapi.modelo.Cliente;
+import algafoodapi.service.AtivacaoClienteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MeuPrimeiroController {
+
+    private AtivacaoClienteService ativacaoClienteService;
+
+    public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+        this.ativacaoClienteService = ativacaoClienteService;
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+
+        Cliente joao = new Cliente("Joao", "joao@gmail.com", "11933335555");
+        ativacaoClienteService.ativar(joao);
+        return "HELLO!";
+    }
+}
---
+++ 2.16. Ambiguidade de beans e injeção de lista de beans/algafood-api/src/main/java/algafoodapi/notificacao/NotificadorEmail.java
@@ -0,0 +1,17 @@
+package algafoodapi.notificacao;
+
+import algafoodapi.modelo.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorEmail implements Notificador {
+
+    @Override
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail() ,mensagem);
+    }
+
+
+
+}
