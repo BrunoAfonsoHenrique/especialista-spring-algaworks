0adf16f109c32e21faab7b4a65e65f9c150f5f8a
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-03 09:30:25 -0300

    codigo-fonte da aula

--- 2.14. Conhecendo os pontos de injeção e a anotação Autowired/algafood-api/src/main/java/algafoodapi/service/AtivacaoClienteService.java
+++ 2.14. Conhecendo os pontos de injeção e a anotação Autowired/algafood-api/src/main/java/algafoodapi/service/AtivacaoClienteService.java
@@ -2,20 +2,32 @@ package algafoodapi.service;
 
 import algafoodapi.modelo.Cliente;
 import algafoodapi.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
+    @Autowired
     private Notificador notificador;
 
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
+//    @Autowired Ponto de injecao (ideal)
+//    public AtivacaoClienteService(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 
-        System.out.println("AtivacaoClienteService: " + notificador);
-    }
+//    public AtivacaoClienteService(String qualquerCoisa) {
+//
+//    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema esta ativo!");
     }
+
+//    @Autowired  Ponto de injecao
+//    public void setNotificador(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 }
