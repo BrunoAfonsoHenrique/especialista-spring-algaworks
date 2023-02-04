3e68ad5a495b42d3b105195ff006c93afd6b989d
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-04 13:11:19 -0300

    codigo-fonte da aula

---
+++ 2.22. Publicando e consumindo eventos customizados/algafood-api/src/main/java/algafoodapi/notificacao/NotificadorEmailMock.java
@@ -0,0 +1,24 @@
+package algafoodapi.notificacao;
+
+import algafoodapi.modelo.Cliente;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("dev")
+@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+@Component
+public class NotificadorEmailMock implements Notificador {
+
+    public NotificadorEmailMock() {
+        System.out.println("Notificador Email MOCK");
+    }
+
+    @Override
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("MOCK: Notificacao seria enviada para %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail() ,mensagem);
+    }
+
+
+
+}
---
+++ 2.22. Publicando e consumindo eventos customizados/algafood-api/src/main/java/algafoodapi/service/AtivacaoClienteService.java
@@ -0,0 +1,34 @@
+package algafoodapi.service;
+
+import algafoodapi.modelo.Cliente;
+import algafoodapi.notificacao.NivelUrgencia;
+import algafoodapi.notificacao.Notificador;
+import algafoodapi.notificacao.TipoDoNotificador;
+import org.springframework.beans.factory.annotation.Autowired;
+
+//@Component
+public class AtivacaoClienteService {
+
+    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+    @Autowired
+    private Notificador notificador;
+
+//    @PostConstruct
+    public void init() {
+        System.out.println("INIT");
+    }
+
+//    @PreDestroy
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
+
+
+    public void ativar(Cliente cliente) {
+        cliente.ativar();
+
+        notificador.notificar(cliente, "Seu cadastro no sistema esta ativo!");
+
+
+    }
+}
