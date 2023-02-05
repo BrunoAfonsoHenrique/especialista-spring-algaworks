6fb83e8d7ce40a5cd53f8475a8aad50288406813
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-05 12:19:57 -0300

    codigo-fonte da aula

---
+++ 2.27. Alterando a configuração do projeto dependendo do ambiente (com Spring Profiles)/algafood-api/src/main/java/algafoodapi/notificacao/NotificadorEmail.java
@@ -0,0 +1,27 @@
+package algafoodapi.notificacao;
+
+import algafoodapi.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+@Component
+public class NotificadorEmail implements Notificador {
+
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+
+    @Override
+    public void notificar(Cliente cliente, String mensagem) {
+
+        System.out.println("HOST: " + host);
+        System.out.println("PORTA: " + porta);
+
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
+    }
+
+}
