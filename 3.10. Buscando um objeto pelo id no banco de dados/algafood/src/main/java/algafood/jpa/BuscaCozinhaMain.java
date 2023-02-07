85d40c9a71e9bfea855725fcdb14792b9f00cdef
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-07 20:56:21 -0300

    codigo-fonte da aula

---
+++ 3.10. Buscando um objeto pelo id no banco de dados/algafood/src/main/java/algafood/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,25 @@
+package algafood.jpa;
+
+import algafood.AlgafoodApplication;
+import algafood.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class BuscaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+
+        System.out.println(cozinha.getNome());
+
+
+    }
+}
