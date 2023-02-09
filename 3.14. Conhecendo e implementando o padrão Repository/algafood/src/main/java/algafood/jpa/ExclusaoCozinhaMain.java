e87f9b907c98921b2df118325e2d1cdd8aee6b00
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-09 20:01:09 -0300

    codigo-fonte da aula

---
+++ 3.14. Conhecendo e implementando o padrão Repository/algafood/src/main/java/algafood/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,23 @@
+package algafood.jpa;
+
+import algafood.AlgafoodApplication;
+import algafood.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class ExclusaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+
+        cadastroCozinha.remover(cozinha);
+
+    }
+}
