f8b219641063de5e93ed1b929169a4e03c69f85b
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-15 16:26:18 -0300

    codigo-fonte da aula

---
+++ 4.15. Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName/algafood/src/main/java/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,32 @@
+package algafood.api.controller;
+
+import algafood.domain.model.Cozinha;
+import algafood.domain.respository.CozinhaRepository;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CozinhaController {
+
+    @Autowired
+    CozinhaRepository cozinhaRepository;
+
+    @GetMapping()
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return cozinhaRepository.buscar(cozinhaId);
+    }
+
+}
---
+++ 4.15. Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName/algafood/src/main/java/algafood/domain/model/Cozinha.java
@@ -0,0 +1,27 @@
+package algafood.domain.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@JsonRootName("cozinha")
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cozinha {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @JsonIgnore
+    @JsonProperty("titulo")
+    @Column(nullable = false)
+    private String nome;
+}
