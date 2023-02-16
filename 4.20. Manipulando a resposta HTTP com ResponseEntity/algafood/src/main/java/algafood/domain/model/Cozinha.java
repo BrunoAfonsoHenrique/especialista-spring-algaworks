64eac937c0bb320c65757345c533238cac55c4a8
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-16 10:50:13 -0300

    codigo-fonte da aula

---
+++ 4.20. Manipulando a resposta HTTP com ResponseEntity/algafood/src/main/java/algafood/domain/model/Cozinha.java
@@ -0,0 +1,25 @@
+package algafood.domain.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@JsonRootName("cozinha")
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cozinha {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonProperty("titulo")
+    @Column(nullable = false)
+    private String nome;
+}
