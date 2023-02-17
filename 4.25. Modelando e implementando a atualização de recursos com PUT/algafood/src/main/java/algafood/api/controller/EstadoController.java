195eb02a0926646e156faa11a735a58933c27dbc
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-17 10:22:29 -0300

    codigo-fonte da aula

---
+++ 4.25. Modelando e implementando a atualização de recursos com PUT/algafood/src/main/java/algafood/api/controller/EstadoController.java
@@ -0,0 +1,30 @@
+package algafood.api.controller;
+
+import algafood.domain.model.Estado;
+import algafood.domain.respository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+    @Autowired
+    EstadoRepository estadoRepository;
+
+    @GetMapping
+    public List<Estado> listar() {
+        return estadoRepository.listar();
+    }
+
+    @GetMapping("/{estadoId}")
+    public Estado buscar(@PathVariable Long estadoId) {
+        return estadoRepository.buscar(estadoId);
+    }
+
+}
---
+++ 4.25. Modelando e implementando a atualização de recursos com PUT/algafood/src/main/java/algafood/domain/model/Cozinha.java
@@ -0,0 +1,24 @@
+package algafood.domain.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@JsonRootName("cozinha")
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cozinha {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+}
