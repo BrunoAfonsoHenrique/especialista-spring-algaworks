169290f69a840eb3e815e265f37bbb709a48832e
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-17 12:06:00 -0300

    codigo-fonte da aula

---
+++ 4.26. Modelando e implementando a exclusão de recursos com DELETE/algafood/src/main/java/algafood/domain/respository/EstadoRepository.java
@@ -0,0 +1,14 @@
+package algafood.domain.respository;
+
+import algafood.domain.model.Estado;
+
+import java.util.List;
+
+public interface EstadoRepository {
+
+    List<Estado> listar();
+    Estado buscar(Long id);
+    Estado salvar(Estado estado);
+    void remover(Estado estado);
+
+}
