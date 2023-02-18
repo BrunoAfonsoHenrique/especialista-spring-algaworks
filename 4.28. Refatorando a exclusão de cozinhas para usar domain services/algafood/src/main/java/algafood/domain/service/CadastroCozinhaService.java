4df20682717ebfa75a3abd65a8d36df299b2c0d1
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-18 12:50:15 -0300

    codigo-fonte da aula

---
+++ 4.28. Refatorando a exclusão de cozinhas para usar domain services/algafood/src/main/java/algafood/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,17 @@
+package algafood.domain.service;
+
+import algafood.domain.model.Cozinha;
+import algafood.domain.respository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroCozinhaService {
+
+    @Autowired
+    CozinhaRepository cozinhaRepository;
+
+    public Cozinha salvar(Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
+    }
+}
