cd965f606c4e4f2a651d21ef284787c81e7e90b5
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-18 13:46:27 -0300

    codigo-fonte da aula

--- 4.29. Desafio modelando e implementando a consulta de recursos de restaurantes/algafood/src/main/java/algafood/api/controller/RestauranteController.java
+++ 4.29. Desafio modelando e implementando a consulta de recursos de restaurantes/algafood/src/main/java/algafood/api/controller/RestauranteController.java
@@ -3,7 +3,10 @@ package algafood.api.controller;
 import algafood.domain.model.Restaurante;
 import algafood.domain.respository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,18 @@ public class RestauranteController {
     @GetMapping
     public List<Restaurante> listar() {
         return restauranteRepository.listar();
+    }
+
+    @GetMapping("/{restauranteId}")
+    public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
+
+        Restaurante restaurante = restauranteRepository.buscar(restauranteId);
+
+        if(restaurante != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(restaurante);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
     }
 
 }
