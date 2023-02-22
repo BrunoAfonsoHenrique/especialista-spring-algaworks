1c829ccd03ee7174c87d8d143b52d4f57c33b22c
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-22 14:21:53 -0300

    codigo-fonte da aula

--- 4.32.01 - estudos spring métodos HTTP - MS/loja/src/main/resources/import.sql
+++ 4.32.01 - estudos spring métodos HTTP - MS/loja/src/main/resources/import.sql
@@ -1,4 +1,5 @@
 insert into camisetas (id, nome, cor, preco, marca) values (1, "Camiseta 1", "azul", "12.60", "adidas");
 insert into camisetas (id, nome, cor, preco, marca) values (2, "Camiseta 2", "vermelho", "158.00", "nike");
 insert into camisetas (id, nome, cor, preco, marca) values (3, "Camiseta 3", "verde", "75.00", "bunny");
+insert into camisetas (id, nome, cor, preco, marca) values (4, "Camiseta 4", "amarelo", "39.90", "nerdland");
 
