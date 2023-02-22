32be60fe4050f50c0db2110b4d31223c73d57912
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-22 20:19:51 -0300

    codigo-fonte da aula

---
+++ 4.34. Finalizando a atualização parcial com a API de Reflections do Spring/algafood/src/main/java/algafood/domain/respository/RestauranteRepository.java
@@ -0,0 +1,14 @@
+package algafood.domain.respository;
+
+import algafood.domain.model.Restaurante;
+
+import java.util.List;
+
+public interface RestauranteRepository {
+
+    List<Restaurante> listar();
+    Restaurante buscar(Long id);
+    Restaurante salvar(Restaurante restaurante);
+    void remover(Restaurante restaurante);
+
+}
