c9896e6d2aeee6037c754f06f5e3499d7fd4a617
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-23 10:47:50 -0300

    codigo-fonte da aula

--- 5.01. Implementando consultas JPQL em repositórios/algafood/src/main/java/algafood/domain/implementation/CozinhaRepositoryImpl.java
+++ 5.01. Implementando consultas JPQL em repositórios/algafood/src/main/java/algafood/domain/implementation/CozinhaRepositoryImpl.java
@@ -4,13 +4,14 @@ import algafood.domain.model.Cozinha;
 import algafood.domain.respository.CozinhaRepository;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
     @PersistenceContext
