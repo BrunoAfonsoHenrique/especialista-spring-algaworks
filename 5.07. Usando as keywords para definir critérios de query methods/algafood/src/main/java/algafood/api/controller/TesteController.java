6b1b6fa44cdb870485730e318e7507601d7dc3a2
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-23 16:08:10 -0300

    codigo-fonte da aula

--- 5.07. Usando as keywords para definir critérios de query methods/algafood/src/main/java/algafood/api/controller/TesteController.java
+++ 5.07. Usando as keywords para definir critérios de query methods/algafood/src/main/java/algafood/api/controller/TesteController.java
@@ -1,12 +1,15 @@
 package algafood.api.controller;
 
 import algafood.domain.model.Cozinha;
+import algafood.domain.model.Restaurante;
 import algafood.domain.respository.CozinhaRepository;
+import algafood.domain.respository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,14 +21,23 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @Autowired
+    RestauranteRepository restauranteRepository;
+
     @GetMapping("/cozinhas/por-nome")
     public List<Cozinha> cozinhasPorNome(String nome) {
-        return cozinhaRepository.findTodasByNome(nome);
+        return cozinhaRepository.findTodasByNomeContaining(nome);
     }
 
-    @GetMapping("/cozinhas/unica-por-nome")
+    @GetMapping("/cozinhas/unica-taxa-frete")
     public Optional<Cozinha> cozinhaPorNome(String nome) {
         return cozinhaRepository.findByNome(nome);
+    }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial,
+                                                     BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
     }
 
 }
