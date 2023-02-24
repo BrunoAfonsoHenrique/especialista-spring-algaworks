3440202747af418900c8e55d6f179ce6197c849e
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-24 10:26:44 -0300

    codigo-fonte da aula

--- 5.08. Conhecendo os prefixos de query methods/algafood/src/main/java/algafood/AlgafoodApplication.java
+++ 5.08. Conhecendo os prefixos de query methods/algafood/src/main/java/algafood/AlgafoodApplication.java
@@ -1,4 +1,4 @@
-package com.algaworks.com.algafood;
+package algafood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ 5.08. Conhecendo os prefixos de query methods/algafood/src/main/java/algafood/api/controller/TesteController.java
@@ -0,0 +1,49 @@
+package algafood.api.controller;
+
+import algafood.domain.model.Cozinha;
+import algafood.domain.model.Restaurante;
+import algafood.domain.respository.CozinhaRepository;
+import algafood.domain.respository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(String nome) {
+        return cozinhaRepository.findTodasByNomeContaining(nome);
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(String nome) {
+        return cozinhaRepository.findByNome(nome);
+    }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantesPorTaxaFrete(
+            BigDecimal taxaInicial, BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("/restaurantes/por-nome")
+    public List<Restaurante> restaurantesPorTaxaFrete(
+            String nome, Long cozinhaId) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+    }
+
+}
