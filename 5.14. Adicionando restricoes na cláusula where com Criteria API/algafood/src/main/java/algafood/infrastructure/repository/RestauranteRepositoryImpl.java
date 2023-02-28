4f0377ca1952da954a877e13b0757f53a0ac3e99
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-28 13:27:49 -0300

    codigo-fonte da aula

--- 5.14. Adicionando restricoes na cláusula where com Criteria API/algafood/src/main/java/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ 5.14. Adicionando restricoes na cláusula where com Criteria API/algafood/src/main/java/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -31,8 +31,11 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
         Root<Restaurante> root = criteria.from(Restaurante.class); // from Restaurante
 
         Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+        Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+        Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
 
-        criteria.where(nomePredicate);
+
+        criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 
         TypedQuery<Restaurante> query = manager.createQuery(criteria);
         return query.getResultList();
