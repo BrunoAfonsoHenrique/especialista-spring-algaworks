c2f9adbb54185b03ad76d1b528094045e4240bcc
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-02-28 10:25:21 -0300

    codigo-fonte da aula

--- 5.12. Implementando uma consulta dinamica com JPQL/algafood/src/main/java/algafood/api/controller/TesteController.java
+++ 5.12. Implementando uma consulta dinamica com JPQL/algafood/src/main/java/algafood/api/controller/TesteController.java
@@ -70,7 +70,9 @@ public class TesteController {
 
     @GetMapping("/restaurantes/por-nome-e-frete")
     public List<Restaurante> restaurantesPorNomeFrete(String nome,
-                                                      BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+                                                      BigDecimal taxaFreteInicial,
+                                                      BigDecimal taxaFreteFinal) {
+        
         return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
     }
 
