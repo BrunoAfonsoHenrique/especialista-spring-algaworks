bcf0b6fdfae3b86a7fba8969b8471c8b9ece70fd
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-03-03 18:59:31 -0300

    codigo-fonte da aula

--- 6.03. Analisando o impacto do relacionamento muitos-para-muitos na REST API/algafood/src/main/java/algafood/domain/model/Restaurante.java
+++ 6.03. Analisando o impacto do relacionamento muitos-para-muitos na REST API/algafood/src/main/java/algafood/domain/model/Restaurante.java
@@ -26,11 +26,12 @@ public class Restaurante {
     @Column(name = "frete_taxa", nullable = false)
     private BigDecimal taxaFrete;
 
-    @JsonIgnore
+//    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
     joinColumns = @JoinColumn(name = "restaurante_id"),
