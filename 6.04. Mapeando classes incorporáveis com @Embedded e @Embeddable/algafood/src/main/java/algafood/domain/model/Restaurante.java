ae15d30b54ab8932d699614afa3f4c4cbda07327
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-03-03 19:19:34 -0300

    codigo-fonte da aula

---
+++ 6.04. Mapeando classes incorporáveis com @Embedded e @Embeddable/algafood/src/main/java/algafood/domain/model/Restaurante.java
@@ -0,0 +1,41 @@
+package algafood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Restaurante {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "frete_taxa", nullable = false)
+    private BigDecimal taxaFrete;
+
+//    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "cozinha_id", nullable = false)
+    private Cozinha cozinha;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento",
+    joinColumns = @JoinColumn(name = "restaurante_id"),
+    inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formasPagamento = new ArrayList<>();
+
+}
