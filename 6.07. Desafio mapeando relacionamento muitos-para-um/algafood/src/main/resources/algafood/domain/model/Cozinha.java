1b9088a896dcdbb74306d37658986c94daec4368
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-03-08 16:44:18 -0300

    codigo-fonte da aula

---
+++ 6.07. Desafio mapeando relacionamento muitos-para-um/algafood/src/main/resources/algafood/domain/model/Cozinha.java
@@ -0,0 +1,30 @@
+package algafood.domain.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonRootName("cozinha")
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cozinha {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<>();
+}
