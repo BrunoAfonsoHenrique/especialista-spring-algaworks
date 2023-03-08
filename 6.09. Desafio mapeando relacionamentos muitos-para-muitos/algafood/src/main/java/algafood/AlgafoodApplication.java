a835cbe4eac2fe4b22c16531680f2773ad34eaee
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-03-08 18:35:29 -0300

    codigo-fonte da aula

--- 6.09. Desafio mapeando relacionamentos muitos-para-muitos/algafood/src/main/java/algafood/AlgafoodApplication.java
+++ 6.09. Desafio mapeando relacionamentos muitos-para-muitos/algafood/src/main/java/algafood/AlgafoodApplication.java
@@ -1,9 +1,12 @@
-package com.algaworks.algafood;
+package algafood;
 
+import algafood.infrastructure.spec.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApplication {
 
 	public static void main(String[] args) {
---
+++ 6.09. Desafio mapeando relacionamentos muitos-para-muitos/algafood/src/main/java/algafood/domain/model/Usuario.java
@@ -0,0 +1,14 @@
+package algafood.domain.model;
+
+import java.time.LocalDateTime;
+
+public class Usuario {
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    private LocalDateTime dataCadastro;
+}
