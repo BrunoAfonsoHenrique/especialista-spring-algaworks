73403967fd30cf20dedacbe09bd0c59ebc570162
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-03-08 16:57:34 -0300

    codigo-fonte da aula

---
+++ 6.08. Desafio mapeando relacionamento um-para-muitos/algafood/src/main/java/algafood/domain/exceptions/EntidadeEmUsoException.java
@@ -0,0 +1,8 @@
+package algafood.domain.exceptions;
+
+public class EntidadeEmUsoException extends RuntimeException {
+
+    public EntidadeEmUsoException(String message) {
+        super(message);
+    }
+}
---
+++ 6.08. Desafio mapeando relacionamento um-para-muitos/algafood/src/main/java/algafood/domain/respository/EstadoRepository.java
@@ -0,0 +1,10 @@
+package algafood.domain.respository;
+
+import algafood.domain.model.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
+}
