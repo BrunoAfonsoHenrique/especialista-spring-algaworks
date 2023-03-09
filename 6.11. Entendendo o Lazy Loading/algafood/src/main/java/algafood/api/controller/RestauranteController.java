8ad410b0f9b183d193a6e73f89eda5d5ecb0fe5c
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-03-09 10:39:32 -0300

    codigo-fonte da aula

--- 6.11. Entendendo o Lazy Loading/algafood/src/main/java/algafood/api/controller/RestauranteController.java
+++ 6.11. Entendendo o Lazy Loading/algafood/src/main/java/algafood/api/controller/RestauranteController.java
@@ -46,6 +46,9 @@ public class RestauranteController {
         System.out.println(restaurantes.get(1).getNome());
         restaurantes.get(1).getFormasPagamento().forEach(System.out::println);
 
+        System.out.println(restaurantes.get(0).getNome());
+        restaurantes.get(0).getFormasPagamento().forEach(System.out::println);
+
         return restaurantes;
     }
 
