e7ac448b698bbc1c26e730b6692e98a2519062fc
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-03-09 11:29:37 -0300

    codigo-fonte da aula

--- 6.12. Alterando a estratégia de fetching para Lazy Loading/algafood/src/main/java/algafood/domain/model/Restaurante.java
+++ 6.12. Alterando a estratégia de fetching para Lazy Loading/algafood/src/main/java/algafood/domain/model/Restaurante.java
@@ -30,7 +30,7 @@ public class Restaurante {
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
-//    @JsonIgnore
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
@@ -53,7 +53,7 @@ public class Restaurante {
     @OneToMany
     private List<Produto> produto = new ArrayList<>();
 
-//    @JsonIgnore
+    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
             joinColumns = @JoinColumn(name = "restaurante_id"),
