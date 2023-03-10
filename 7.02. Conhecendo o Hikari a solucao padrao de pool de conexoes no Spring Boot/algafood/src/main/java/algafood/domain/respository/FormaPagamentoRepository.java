3f05566fd383313f04344834691ddf0dc0e968b2
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-03-10 13:25:07 -0300

    codigo-fonte da aula

---
+++ 7.02. Conhecendo o Hikari a solucao padrao de pool de conexoes no Spring Boot/algafood/src/main/java/algafood/domain/respository/FormaPagamentoRepository.java
@@ -0,0 +1,9 @@
+package algafood.domain.respository;
+
+import algafood.domain.model.FormaPagamento;
+
+import java.util.List;
+
+public interface FormaPagamentoRepository {
+
+}
---
+++ 7.03. Configurando o pool de conexões do Hikari/algafood/src/main/java/algafood/AlgafoodApplication.java
@@ -0,0 +1,16 @@
+package algafood;
+
+import algafood.infrastructure.repository.CustomJpaRepositoryImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
+public class AlgafoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApplication.class, args);
+	}
+
+}
